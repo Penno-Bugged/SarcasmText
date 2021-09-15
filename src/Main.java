50a7ed15a6fab10934c667d17524dd49a823f98d
Chris Penno <cp117@students.waikato.ac.nz>
2021-09-15 23:04:58 +1200

    Made code executable

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,41 @@
 import java.util.*;
+import java.io.IOException;
+import java.awt.Toolkit;
+import java.awt.datatransfer.*;
 
 public class Main {
 
     /**
      * Method to get user input
      * Checks accuracy by immediately outputting the string
-     * @return The string entered by the user to main();
      */
-    static String userInput(){
-        Scanner sc= new Scanner(System.in);                 //System.in is a standard input stream
-        System.out.print("Enter a string: ");
-        String str= sc.nextLine();                          //Reads string
-        System.out.print("You have entered: "+str+"\n");
-        return str;                                         //Returns string to main();
-
+    static void userInput(){
+        try{
+            Clipboard clip =Toolkit.getDefaultToolkit().getSystemClipboard();
+            // Get data stored in the clipboard that is in the form of a string (text)
+            String str = (String)clip.getData(DataFlavor.stringFlavor);
+            halfRandom(str);
+        } catch (IOException | UnsupportedFlavorException e) {
+            e.printStackTrace();
+        }
     }
 
 
     /**
      * Method to alternate capitalisation and perform this action
      * @param text - the string from the userInput method
-     * @return Returns a string with the changes already made
      */
-    static String halfRandom(String text) {
+    static void halfRandom(String text) {
         List<String> textList = new ArrayList<>();          //Creates list array to store characters
         String[] arrOfStr = text.split("", 0);   //Separates the string at each character
         for (int i = 0; i < arrOfStr.length; i++) {         //Iterates through the String[] and increase a counter
             if (i % 2 == 0) {                               //If counter is even capitalise the letter
                 textList.add(arrOfStr[i].toUpperCase());
-            } else textList.add(arrOfStr[i]);
+            } else textList.add(arrOfStr[i].toLowerCase());
         }
-        return String.join("", textList);           //Returns the string to main();
+        StringSelection selection = new StringSelection(String.join("", textList));
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(selection, selection);
     }
 
 
@@ -39,6 +44,6 @@ public class Main {
      * @param args None - no console call
      */
     public static void main(String[] args) {
-        System.out.println(halfRandom(userInput()));          //Prints the output string with pure random capitalisation
+        userInput();          //Prints the output string with pure random capitalisation
     }
 }
